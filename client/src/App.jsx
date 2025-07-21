cd081f9a58a78b1a1fdb4f4b8c79dec0e1f4864b
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-07-21 13:17:32 +0300

    added admin dashboard: navbar, sidebar, add shows, layout, list bookings and list shows

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
@@ -7,7 +7,12 @@ import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import Footer from './components/Footer'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows' 
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 import { Toaster } from "react-hot-toast"
+import { Layout } from 'lucide-react'
 
 const App = () => {
 
@@ -25,6 +30,12 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout />} />
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favorites' element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>          
+          <Route path='add-shows' element={<AddShows/>}/>
+          <Route path='list-shows' element={<ListShows/>}/>
+          <Route path='list-bookings' element={<ListBookings/>}/>
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer/>}
       
